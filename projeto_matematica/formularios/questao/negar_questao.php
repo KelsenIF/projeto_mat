c81febc1f62615f4fd70a84980f26ca4eed99495
gbsinIF <gabriel.borges@alunos.ifsuldeminas.edu.br>
2025-09-18 16:49:03 -0300

    Add files via upload

---
+++ projeto_matematica/formularios/questao/negar_questao.php
@@ -0,0 +1,14 @@
+<?php
+include_once('../../Include/connection.php');
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "UPDATE questoes SET id_situacao = 0 WHERE id = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+
+    // Redireciona de volta para a página de questões em análise
+    header('Location: ../../../projeto_matematica/formularios/questao/questoes_em_analise.php');
+    exit();
+}
+?>
