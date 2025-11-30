64687459c20d77256339cc53b7669dc302b0bc2d
gbsinIF <gabriel.borges@alunos.ifsuldeminas.edu.br>
2025-11-30 19:35:36 -0300

    Delete Projeto_Matemática directory

--- Projeto_Matemática/DASHBOARDS/COMPONENTES/footer.php
+++
@@ -1,8 +0,0 @@
-
-<footer class="footer py-3 bg-dark  border-top">
-    <div class="container-fluid text-center">
-        <span class="text-muted">
-            &copy; <?php echo date("Y/m/d"); ?>
-        </span>
-    </div>
-</footer>
